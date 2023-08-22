853a065a920f10cc7187d4dacd2b3119069940c3
Em Evgeniy <emevgeniy1323@gmail.com>
2023-08-23 02:03:04 +0500

    push

--- api/tg/Telegram.js
+++ api/tg/Telegram.js
@@ -7,58 +7,60 @@ const botToken = "6526594197:AAF3YJQCuKx4lvXy3GUDjqgYUDIrAZYc9fU";
 const bot = new TelegramBot(botToken, { polling: true });
 
 const mainMenuKeyboard = [
-  ["📃 Список городов", "📖 Описание городов"],
-  ["🚚 Мои заказы", "🛒 Заказать"],
-  ["🧑‍💼 Проконсультироваться"],
+	["📃 Список городов", "📖 Описание городов"],
+	["🚚 Мои заказы", "🛒 Заказать"],
+	["🧑‍💼 Проконсультироваться"],
 ];
 
-const backKeyboard = [
-  ["Назад"]
-];
+const backKeyboard = [["Назад"]];
 
 const setupTelegramBot = () => {
-  const userStack = []; // Стек для хранения предыдущих меню
-  bot.on("text", async (msg) => {
-    const cities = await CityModel.find();
-    try {
-      if (msg.text.startsWith("/start")) {
-        userStack.length = 0; // Очищаем стек при старте бота
-        await bot.sendMessage(msg.chat.id, "Текст: Выберите экскурсию👇", {
-          reply_markup: {
-            keyboard: mainMenuKeyboard,
-            resize_keyboard: true,
-          },
-        });
-      } else if (msg.text == "📃 Список городов") {
-        userStack.push(mainMenuKeyboard); // Сохраняем текущее меню в стеке
-        await bot.sendMessage(msg.chat.id, "Список городов", {
-          reply_markup: {
-            keyboard: [cities.map((el) => el.title), backKeyboard],
-						resize_keyboard:true
-          },
-        });
-      } else if (msg.text == "Назад" && userStack.length > 0) {
-        const prevMenu = userStack.pop(); // Извлекаем предыдущее меню из стека
-        await bot.sendMessage(msg.chat.id, "Вы вернулись назад", {
-          reply_markup: {
-            keyboard: prevMenu,
-            resize_keyboard: true,
-          },
-        });
-      } else if (msg.text == "📖 Описание городов") {
-        userStack.push(mainMenuKeyboard); // Сохраняем текущее меню в стеке
-        await bot.sendMessage(msg.chat.id, "Список городов", {
-          reply_markup: {
-            keyboard: [cities.map((el) => el.title), backKeyboard],
-          },
-        });
-      } else if (cities.some((city) => city.title === msg.text)) {
-        // ... остальной код
-      }
-    } catch (e) {
-      console.log(e);
-    }
-  });
+	const userStack = []; // Стек для хранения предыдущих меню
+	bot.on("text", async (msg) => {
+		const cities = await CityModel.find();
+		try {
+			if (msg.text.startsWith("/start")) {
+				userStack.length = 0; // Очищаем стек при старте бота
+				await bot.sendMessage(msg.chat.id, "Текст: Выберите экскурсию👇", {
+					reply_markup: {
+						keyboard: mainMenuKeyboard,
+						resize_keyboard: true,
+					},
+				});
+			} else if (msg.text == "📃 Список городов") {
+				userStack.push(mainMenuKeyboard); // Сохраняем текущее меню в стеке
+				await bot.sendMessage(msg.chat.id, "Список городов", {
+					reply_markup: {
+						keyboard: [cities.map((el) => el.title), ...backKeyboard],
+						resize_keyboard: true,
+					},
+				});
+			} else if (msg.text == "Назад" && userStack.length > 0) {
+				const prevMenu = userStack.pop(); // Извлекаем предыдущее меню из стека
+				await bot.sendMessage(msg.chat.id, "Вы вернулись назад", {
+					reply_markup: {
+						keyboard: prevMenu,
+						resize_keyboard: true,
+					},
+				});
+			} else if (msg.text == "📖 Описание городов") {
+				userStack.push(mainMenuKeyboard); // Сохраняем текущее меню в стеке
+				await bot.sendMessage(msg.chat.id, "Список городов", {
+					reply_markup: {
+						keyboard: [cities.map((el) => el.title), ...backKeyboard],
+					},
+				});
+			} else if (cities.some((city) => city.title === msg.text)) {
+				await bot.sendMessage(msg.chat.id, `Вы выбрали  ${msg.text}`, {
+					reply_markup: {
+						keyboard: [cities.map((el) => el.lang), ...backKeyboard],
+					},
+				});
+			}
+		} catch (e) {
+			console.log(e);
+		}
+	});
 };
 
 export default setupTelegramBot;
